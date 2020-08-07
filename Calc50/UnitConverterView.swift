a14f9b774b51734c5e081084cc8205f624957b77
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-07 14:39:54 +0200

    created all view controllers

---
+++ Calc50/UnitConverterView.swift
@@ -0,0 +1,30 @@
+//
+//  UnitConverterView.swift
+//  Calc50
+//
+//  Created by Bheki Maenetja on 2020/08/07.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class UnitConverterView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
